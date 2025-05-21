7c2c5d2fc102d2f9e69e69989aee202bf45a530c
Rain-Alkai <ra2005090@qu.edu.qa>
2025-05-21 13:06:11 +0300

    a

--- 3D_AI_Avatar_frontend/vite.config.js
+++ 3D_AI_Avatar_frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/DeepSona/', // 👈 MUST match the repo name exactly (case-sensitive!)
   plugins: [react()],
-  base: '/Guide/',
 });
